e4f4cddbc0e40dddd9d2aecaf6986e530fc6713d
Richard <xqtwkk@gmail.com>
2023-04-23 23:43:32 +0300

    3

--- src/main/java/com/epam/mjc/collections/set/SetCombinationCreator.java
+++ src/main/java/com/epam/mjc/collections/set/SetCombinationCreator.java
@@ -2,8 +2,24 @@ package com.epam.mjc.collections.set;
 
 import java.util.HashSet;
 import java.util.Set;
+import java.util.TreeSet;
 
 public class SetCombinationCreator {
     public Set<String> createSetCombination(Set<String> firstSet, Set<String> secondSet, Set<String> thirdSet) {
+        HashSet<String> set = new HashSet<>();
+        Set<String> intersection = new HashSet<>(firstSet);
+        intersection.retainAll(secondSet);
+
+        for (String element : intersection) {
+            if (!thirdSet.contains(element)) {
+                set.add(element);
+            }
+        }
+
+        for (String key : thirdSet) {
+            if (!firstSet.contains(key) && !secondSet.contains(key)) set.add(key);
+        }
+
+        return set;
     }
 }
