a31aa2b63bb3cefc5290de6a1d58f07e814753f3
Richard <xqtwkk@gmail.com>
2023-04-23 23:18:58 +0300

    1

--- src/main/java/com/epam/mjc/collections/set/HashSetCreator.java
+++ src/main/java/com/epam/mjc/collections/set/HashSetCreator.java
@@ -5,5 +5,17 @@ import java.util.List;
 
 public class HashSetCreator {
     public HashSet<Integer> createHashSet(List<Integer> sourceList) {
+        HashSet<Integer> set = new HashSet<>();
+        for (Integer number : sourceList) {
+            if (number == 0 || number % 2 == 0) {
+                while (number % 2 == 0) {
+                    set.add(number);
+                    number /= 2;
+                }
+            }
+            set.add(number);
+            set.add(number * 2);
+        }
+        return set;
     }
 }
