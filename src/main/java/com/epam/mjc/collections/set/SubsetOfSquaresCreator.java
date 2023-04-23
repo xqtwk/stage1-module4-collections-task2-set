918e3f7b2b2376a34f6f5880393aef9d5d52d352
Richard <xqtwkk@gmail.com>
2023-04-23 23:23:56 +0300

    2

--- src/main/java/com/epam/mjc/collections/set/SubsetOfSquaresCreator.java
+++ src/main/java/com/epam/mjc/collections/set/SubsetOfSquaresCreator.java
@@ -1,9 +1,19 @@
 package com.epam.mjc.collections.set;
 
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.TreeSet;
 
 public class SubsetOfSquaresCreator {
     public Set<Integer> createSubsetOfSquares(List<Integer> sourceList, int lowerBound, int upperBound) {
+        TreeSet<Integer> set = new TreeSet<>();
+        for (int key : sourceList) {
+            int square = key * key;
+            if (square >= lowerBound && square <= upperBound) {
+                set.add(square);
+            }
+        }
+        return set;
     }
 }
